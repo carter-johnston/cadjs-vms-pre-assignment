0ecaab3349bed95af1f3daedd08b77d628e634ff
Carter <johnston.j.carter@gmail.com>
2022-08-20 23:49:16 -0400

    Added opportunities list

--- routes/opportunity.js
+++ routes/opportunity.js
@@ -4,7 +4,13 @@ const router = express.Router();
 module.exports = () => {
     router.get('/', (req, res) =>{
         //TODO: Authorize "is user signed in? NO -> route to login"
-        res.render('opportunity');
+
+        let oppList = [//TEMP: hardcoded values to be replaced
+            {organization: 'OceanSyde', desc: 'beach cleanup', date: '12/1/2023'},
+            {organization: 'Everhills', desc: 'homeless shelter', date: '12/5/2023'},
+            {organization: 'VMSC', desc: 'foodline service', date: '12/22/2023'},
+        ]
+        res.render('opportunity', {oppList: oppList});
     });
     return router;
 };
