56618703de2f1d8e1576c9dbf48cefeea2ae9455
Carter <johnston.j.carter@gmail.com>
2022-08-21 01:32:03 -0400

    Added search to oppotunities list

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cookieSession({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static('resources'));
 app.use('/', routes());
 
 app.listen(3000, () =>
