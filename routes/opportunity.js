87b659c4a18a1a46936af5c456010599fad21572
Carter <johnston.j.carter@gmail.com>
2022-08-22 22:52:28 -0400

    added "created-by" filter wip

--- routes/opportunity.js
+++ routes/opportunity.js
@@ -6,9 +6,9 @@ module.exports = () => {
         //TODO: Authorize "is user signed in? NO -> route to login"
 
         let oppList = [//TEMP: hardcoded values to be replaced
-            {id: 11, organization: 'OceanSyde', desc: 'beach cleanup', date: '12/1/2023'},
-            {id: 22, organization: 'Everhills', desc: 'homeless shelter', date: '12/5/2023'},
-            {id: 33, organization: 'VMSC', desc: 'foodline service', date: '12/22/2023'},
+            {id: 11, organization: 'OceanSyde', desc: 'beach cleanup', dateCreated:'08/20/2022', date: '12/1/2023'},
+            {id: 22, organization: 'Everhills', desc: 'homeless shelter', dateCreated:'08/14/2022', date: '12/5/2023'},
+            {id: 33, organization: 'VMSC', desc: 'foodline service', dateCreated:'08/20/2021', date: '12/22/2023'},
         ]
         res.render('opportunity', {oppList: oppList});
     });
